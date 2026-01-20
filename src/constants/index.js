9a8267318dbd20f32c9c3c9f2a12245568d4f51f
Vihang Jadhav <vihang.jadhav155@gmail.com>
2026-01-20 20:35:58 +0530

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,8 @@
+export const navLinks = [
+            { label: "Store" },
+            { label: "Mac" },
+            { label: "iPhones" },
+            { label: "Watch" },
+            { label: "Vision" },
+            { label: "Airpods" },
+          ];
